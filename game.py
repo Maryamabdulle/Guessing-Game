188140635a1b486b28734fc4bf9a2090ed138543
Maryam Abdulle <iammaryamabdulle@gmail.com>
2022-12-16 21:23:48 -0700

    added more features

--- game.py
+++ game.py
@@ -1,32 +1,46 @@
 """A number-guessing game."""
 
-from mimetypes import guess_all_extensions
 import random
-number = random.randint (1, 100)
-# print("Howdy, what's your name? ")
-name= input ("Howdy, what's your name? ")
-print( f"{name}, I'm thinking of a number between 1 and 100.")
-print("Try to guess my number.")
 
-number_of_guesses = 0
+def guessing(low=1, high=100):
+    """plays a number guessing game.
+Asks the player to identify themselevs and make a series of guesses as to 
+which integer between 1-100 (or bewteen optional parameters) was generated .
+Tells player if the guess is high or low or correct and how mnay guesses it took."""
 
-while True:
-    guess = int (input ("Your guess: "))
-
-    if guess < number:
-        print("Your guess is too low, try again.")
-        number_of_guesses +=1 
-    if  guess > number:
-        print( "Your guess is too high, try again.")
-        number_of_guesses +=1 
-
-    if guess == number:
-     
-        print(f"Well done {name}! You found my number in {number_of_guesses} tries!")
-        break
+    name = input ("Howdy, what's your name? ")
+    print( f"{name}, I'm thinking of a number between {low} and {high}.")
+    print ("Try to guess my number.")
+    best_scores = None
 
 
+    while True:
+        count = 0
+        guess = 0
+        num = random.randint(low, high)
+    
+        while guess != num:
+            try:
+                guess = int(input("Your guess? "))
+                count += 1
+            except:
+                print("Error")
+                continue
 
+            print (f"{guess}?")
 
+            if guess < num:
+                print("Your guess is too low, try again. ")
+            if  guess > num:
+                print("Your guess is too high, try again. ")
+    
+        print(f"Well done {name}! You found my number in {count} tries!")
+        if best_score== None or best_score > count:
+            best_score = count
+        print(f"Your best score is {best_score}.")
+        ask= input("Do you want to play again? y/n")
+        if ask == 'n':
+            return best_score
 
-# Put your code here
+guessing()
+
